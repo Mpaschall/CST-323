d483ce6a3630dcb004eed04d6473171306996ff7
Mathew Paschall <XDarkZero89@hotmail.com>
2019-05-06 00:43:55 -0400

    Update dbFunctions.php

--- utility/dbFunctions.php
+++ utility/dbFunctions.php
@@ -1,12 +1,7 @@
 <?php
 
 function dbConnect(){
-    private $dbServerName = "l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-    private $dbUserName = "kckc4cm3ga7jyz88";
-    private $dbPassword = "pg4kil9m4244mp77";
-    private $dbName = "w6xh268xdx236rsp";
-    
-    $connection = new mysqli($this->$dbServerName, $this->$dbUserName, $this->$dbPassword , $this->$dbName);
+    $connection = new mysqli("l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "kckc4cm3ga7jyz88", "pg4kil9m4244mp77" , "w6xh268xdx236rsp");
     if ($connection->connect_errno)
     {
         die("Connection Failed.");

1f3f4feda6bbc304147bb94e7e2d459fb92d56a2
Mathew Paschall <XDarkZero89@hotmail.com>
2019-05-05 23:04:27 -0400

    Update dbFunctions.php

--- utility/dbFunctions.php
+++ utility/dbFunctions.php
@@ -1,10 +1,15 @@
 <?php
 
+private $dbServerName = l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;
+private $dbUserName = kckc4cm3ga7jyz88;
+private $dbPassword = pg4kil9m4244mp77;
+private $dbName = w6xh268xdx236rsp;
+
 function dbConnect(){
-    $connection = new mysqli("localhost", "root", "root" , "eCommerce", 8889);
+    $connection = new mysqli($this->$dbServerName, $this->$dbUserName, $this->$dbPassword , $this->$dbName);
     if ($connection->connect_errno)
     {
-        $message = "Connection Failed.";
+        echo "Connection Failed.";
 
         include('connectionFailed.php');
     }

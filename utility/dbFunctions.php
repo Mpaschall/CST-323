0912b4d7584c8996128f8415120527b13dce758c
Mathew Paschall <XDarkZero89@hotmail.com>
2019-05-09 01:27:30 -0400

    Update dbFunctions.php

--- utility/dbFunctions.php
+++ utility/dbFunctions.php
@@ -4,7 +4,7 @@ function dbConnect(){
     $connection = new mysqli("l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "kckc4cm3ga7jyz88", "pg4kil9m4244mp77" , "w6xh268xdx236rsp");
     if ($connection->connect_errno)
     {
-        die("Connection Failed.");
+        echo "Connection Failed.";
     }
     else
     {
